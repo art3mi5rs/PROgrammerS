cf6a1187684b1a035b5bd15184ccef3db12632d0
Shachaf <shachaf.smith@gmail.com>
2020-05-01 13:30:15 -0700

    Update Panel.java

--- src/lab_finalcapstoneproject/Panel.java
+++ src/lab_finalcapstoneproject/Panel.java
@@ -1,5 +1,10 @@
 package lab_finalcapstoneproject;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+import javax.swing.*;
+
 import javax.swing.JFrame;
 
 public class Panel {
@@ -9,7 +14,7 @@ public class Panel {
     JFrame w = new JFrame("Simple Window");
     w.setBounds(100, 100, 640, 480);
     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    SimpleWindow panel = new SimpleWindow();
+    Panel panel = new Panel();
     w.addKeyListener(panel);
     w.add(panel);
     w.setResizable(true);
