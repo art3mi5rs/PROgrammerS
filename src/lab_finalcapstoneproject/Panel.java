56ae82ba4d5dd5c7172bf2d1eddd576e56b66302
Mana Nagampalli <manathestar@gmail.com>
2020-05-01 13:30:40 -0700

    Update Panel.java

--- src/lab_finalcapstoneproject/Panel.java
+++ src/lab_finalcapstoneproject/Panel.java
@@ -4,17 +4,4 @@ import javax.swing.JFrame;
 
 public class Panel {
   
-  public static void main(String[] args)
-  {
-    JFrame w = new JFrame("Simple Window");
-    w.setBounds(100, 100, 640, 480);
-    w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    SimpleWindow panel = new SimpleWindow();
-    w.addKeyListener(panel);
-    w.add(panel);
-    w.setResizable(true);
-    w.setVisible(true);
-    
-    panel.run();
-  }
 }
