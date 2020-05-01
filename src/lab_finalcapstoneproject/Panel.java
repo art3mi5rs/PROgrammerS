edab6fa8802a013fefd3885b21caa369cda8e3c0
Shachaf <shachaf.smith@gmail.com>
2020-05-01 11:29:30 -0700

    make window

--- src/lab_finalcapstoneproject/Panel.java
+++ src/lab_finalcapstoneproject/Panel.java
@@ -1,5 +1,20 @@
 package lab_finalcapstoneproject;
 
-public class Panel {
+import javax.swing.JFrame;
 
+public class Panel {
+  
+  public static void main(String[] args)
+  {
+    JFrame w = new JFrame("Simple Window");
+    w.setBounds(100, 100, 640, 480);
+    w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    SimpleWindow panel = new SimpleWindow();
+    w.addKeyListener(panel);
+    w.add(panel);
+    w.setResizable(true);
+    w.setVisible(true);
+    
+    panel.run();
+  }
 }
