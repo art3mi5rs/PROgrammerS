eae6fa6d8fe61d0db04743462ae25f57798144d5
Shachaf <shachaf.smith@gmail.com>
2020-05-01 13:46:15 -0700

    this is bad

--- src/lab_finalcapstoneproject/Panel.java
+++ src/lab_finalcapstoneproject/Panel.java
@@ -2,22 +2,17 @@ package lab_finalcapstoneproject;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.AffineTransform;
-import javax.swing.*;
-
 import javax.swing.JFrame;
 
 public class Panel {
   
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) {
     JFrame w = new JFrame("Panel");
     w.setBounds(100, 100, 640, 480);
     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     Panel panel = new Panel();
     //panel.setBackground(Color.WHITE);
     Container c = w.getContentPane();
-    c.add(panel);
     w.setVisible(true);
   }
 }
