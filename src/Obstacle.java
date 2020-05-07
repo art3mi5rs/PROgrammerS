c5e729659bc4a0fb54b16b6d7b3affdce166265d
Mana Nagampalli <manathestar@gmail.com>
2020-05-06 17:05:13 -0700

    go

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -3,7 +3,7 @@
 //Ver:1
 //Notes: This class is a subclass of sprite and extends all methods in sprite and conains the additional code for 
 //obstacle in game
-package lab_finalcapstoneproject;
+
 
 public class Obstacle extends Sprite {
 
--- src/Sprite.java
+++ src/Sprite.java
@@ -3,7 +3,6 @@
 //Ver:1
 //Notes: This class is the superclass of player and obstacle class and it provides the methods that lets them print
 //themselves and collision detection
-package lab_finalcapstoneproject;
 
 public abstract class Sprite {
 
