4bbea804bf4522e1c74ae04e12c1837ec0b6d0df
Shachaf <shachaf.smith@gmail.com>
2020-05-06 17:24:23 -0700

    no weird stuff

--- src/Player.java
+++ src/Player.java
@@ -3,10 +3,6 @@
 //Ver:1
 //Notes: This class is a subclass of sprite and extends all methods in sprite and contains the additional code for 
 //player in game
-<<<<<<< HEAD
-
-=======
->>>>>>> 22b338eb5e862cf6a85bece1d4b43a827ab8b09c
 
 public class Player extends Sprite {
   // need to add action listener for 'up' or space key to jump
