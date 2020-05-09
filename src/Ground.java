fa66cee30f984aa0060b43a5f7fd472bea313fc6
Mana Nagampalli <manathestar@gmail.com>
2020-05-09 10:02:36 -0700

    go

--- src/Ground.java
+++ src/Ground.java
@@ -17,7 +17,7 @@ public class Ground extends Sprite {
 		
 		// CONSTRUCTOR
 		public Ground(int x, int y) {
-			super("grass.png",x,y,780,50);
+			super("grass.png",x,y,780,200);
 			orgX = x;
 			curX = x;
 			orgY = y;
