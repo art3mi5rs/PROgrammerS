4e3f1cc0f133e06e0b8068ab8c836c008de825e1
Mana Nagampalli <manathestar@gmail.com>
2020-05-09 11:15:19 -0700

    go

--- src/Ground.java
+++ src/Ground.java
@@ -6,47 +6,19 @@
 
 public class Ground extends Sprite {
 
-  // This is the ground
+	// This is the ground
 
-  private int orgX;
-  private int orgY;
-  private double curX;
+	private int orgX;
+	private int orgY;
+	private double curX;
 
-  // Constructor
-  public Ground(int x, int y) {
-    super("grass.png", x, y - 100, 780, 150);
-    orgX = x;
-    curX = x;
-    orgY = y;
+	// Constructor
+	public Ground(int x, int y) {
+		super("grass.png", x, y - 100, 780, 150);
+		orgX = x;
+		curX = x;
+		orgY = y;
 
-  }
-
-
-		private int orgX;
-		private int orgY;
-		private double curX;
-		
-		// CONSTRUCTOR
-		public Ground(int x, int y) {
-			super("grass.png",x,y-100,780,200);
-			orgX = x;
-			curX = x;
-			orgY = y;
-		
-			
-		}
-		
-		// METHODS
-		public void circularLeftShift() {
-			if (curX >= 0) {
-				moveByAmount(1, 0);
-				curX -= 1;
-			}
-			else{
-				moveToLocation(orgX, orgY);
-				curX = orgX;
-			}
-		}
-
+	}
 
 }
