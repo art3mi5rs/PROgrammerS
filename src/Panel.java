dcd7b5fd302dcab984c06233821481140a1ad01b
Mana Nagampalli <manathestar@gmail.com>
2020-05-22 20:30:10 -0700

    go

--- src/Panel.java
+++ src/Panel.java
@@ -15,262 +15,280 @@ import java.util.TimerTask;
 
 public class Panel extends JPanel implements KeyListener {
 
-  // Fields
-  public static final int DRAWING_WIDTH = 800;
-  public static final int DRAWING_HEIGHT = 600;
+	// Fields
+	public static final int DRAWING_WIDTH = 800;
+	public static final int DRAWING_HEIGHT = 600;
 
-  private Player mahaf;
-  private Obstacle virus;
-  private Mask mask;
-  private Clouds cloud;
-  private Clouds cloud1;
-  private Clouds cloud2;
-  private Clouds cloud3;
-  private Ground grass;
-  private Timer virusTimer;
-  private Timer maskTimer;
-  private Timer cloudsTimer;
-  private Timer pointsTimer;
-  private Music background;
-  private Music gameOver;
+	private Player mahaf;
+	private Obstacle virus;
+	private Mask mask;
+	private Clouds cloud;
+	private Clouds cloud1;
+	private Clouds cloud2;
+	private Clouds cloud3;
+	private Ground grass;
+	private Timer virusTimer;
+	private Timer maskTimer;
+	private Timer cloudsTimer;
+	private Timer pointsTimer;
+	private Timer backgroundTimer;
+	private Music background;
+	private Music gameOver;
 
-  private boolean collision;
-  private boolean hasMask;
-  private int points;
+	private boolean collision;
+	private boolean hasMask;
+	private int points;
 
-  // Constructor
-  public Panel() {
-    super();
-    collision = false;
-    hasMask = false;
+	// Constructor
+	public Panel() {
+		super();
+		collision = false;
+		hasMask = false;
 
-    mahaf = new Player(40, 480);
-    virus = new Obstacle(740, 480);
-    mask = new Mask(660, 480);
-    cloud = new Clouds(740, 20);
-    cloud1 = new Clouds(900, 20);
-    cloud2 = new Clouds(1060, 80);
-    cloud3 = new Clouds(1200, 50);
-    grass = new Ground(0, 520);
-    mahaf = new Player(230, 480);
-    virus = new Obstacle(780, 480);
-    virusTimer = new Timer("virusTimer");
-    maskTimer = new Timer("maskTimer");
-    cloudsTimer = new Timer("cloudsTimer");
-    pointsTimer = new Timer("pointsTimer");
-    background = new Music("background.wav");
-    gameOver= new Music("gameOver.wav");
-    
-    setBackground(Color.CYAN);
+		mahaf = new Player(40, 480);
+		virus = new Obstacle(740, 480);
+		mask = new Mask(660, 480);
+		cloud = new Clouds(740, 20);
+		cloud1 = new Clouds(900, 20);
+		cloud2 = new Clouds(1060, 80);
+		cloud3 = new Clouds(1200, 50);
+		grass = new Ground(0, 520);
+		mahaf = new Player(230, 480);
+		virus = new Obstacle(780, 480);
+		virusTimer = new Timer("virusTimer");
+		maskTimer = new Timer("maskTimer");
+		cloudsTimer = new Timer("cloudsTimer");
+		pointsTimer = new Timer("pointsTimer");
+		backgroundTimer = new Timer("backgroundTimer");
+		background = new Music("background.wav");
+		gameOver = new Music("gameOver.wav");
 
-  }
+		setBackground(Color.CYAN);
 
-  // Runs all of the methods needed
-  public void runWithTimer() {
-    runVirus();
-    runMask();
-    runClouds(cloud, 55L);
-    runClouds(cloud1, 42L);
-    runClouds(cloud2, 49L);
-    runClouds(cloud3, 30L);
-    runPoints();
-    if (collision!=true){
-    background.play();
-    }
-    
+	}
 
-  }
+	// Runs all of the methods needed
+	public void runWithTimer() {
+		runVirus();
+		runMask();
+		runClouds(cloud, 55L);
+		runClouds(cloud1, 42L);
+		runClouds(cloud2, 49L);
+		runClouds(cloud3, 30L);
+		runPoints();
+		runBackgroundMusic();
+	}
+	
+	// This method is called in runBackgroundMusic, and is in charge of running code
+		// related to the background music 
+		private void runBackgroundMusic() {
+			TimerTask musicTask = new TimerTask() {
+				@Override
+				public void run() {
+					background.play();
+					collision = checkCollision();
+					if (collision) {
+						backgroundTimer.cancel();
+					}
+					repaint();
+				}
 
-//This method is called in runWithTimer, and is in charge of running code
-  // related to the virus
-  private void runVirus() {
-    TimerTask virusTask = new TimerTask() {
-      @Override
-      public void run() {
-        virus.circularleftShift();
-        collision = checkCollision();
-        if (collision) {
-          virusTimer.cancel();
-        }
-        repaint();
-      }
+			};
+			//the period was made 10 millisecinds to make the music appear 
+			//like it is going on forever
+			backgroundTimer.scheduleAtFixedRate(musicTask, 0, 10);
+		}
 
-    };
-    virusTimer.scheduleAtFixedRate(virusTask, 1000L, 10L);
-  }
+	// This method is called in runWithTimer, and is in charge of running code
+	// related to the virus
+	private void runVirus() {
+		TimerTask virusTask = new TimerTask() {
+			@Override
+			public void run() {
+				virus.circularleftShift();
+				collision = checkCollision();
+				if (collision) {
+					virusTimer.cancel();
+				}
+				repaint();
+			}
 
-//This method is called in runWithTimer, and is in charge of running code
-// related to the mask
-  private void runMask() {
-    TimerTask maskTask = new TimerTask() {
-      @Override
-      public void run() {
-        mask.circularleftShift();
-        hasMask = checkMaskCollision();
-        if (collision) {
-          maskTimer.cancel();
-        }
-        repaint();
-      }
+		};
+		virusTimer.scheduleAtFixedRate(virusTask, 1000L, 10L);
+	}
 
-    };
-    maskTimer.scheduleAtFixedRate(maskTask, 3800L, 10L);
-  }
+	// This method is called in runWithTimer, and is in charge of running code
+	// related to the mask
+	private void runMask() {
+		TimerTask maskTask = new TimerTask() {
+			@Override
+			public void run() {
+				mask.circularleftShift();
+				hasMask = checkMaskCollision();
+				if (collision) {
+					maskTimer.cancel();
+				}
+				repaint();
+			}
 
-  // This method is called in runWithTimer, and is in charge of running code
-  // related to the clouds
-  private void runClouds(Clouds c, long l) {
-    TimerTask cloudsTask = new TimerTask() {
-      @Override
-      public void run() {
-        c.circularLeftShift();
-        repaint();
-      }
+		};
+		maskTimer.scheduleAtFixedRate(maskTask, 3800L, 10L);
+	}
 
-    };
-    cloudsTimer.scheduleAtFixedRate(cloudsTask, 1000L, l);
-  }
+	// This method is called in runWithTimer, and is in charge of running code
+	// related to the clouds
+	private void runClouds(Clouds c, long l) {
+		TimerTask cloudsTask = new TimerTask() {
+			@Override
+			public void run() {
+				c.circularLeftShift();
+				repaint();
+			}
 
-  // This method is called in runWithTimer, and is in charge of running code
-  // related to the points system
-  private void runPoints() {
-    TimerTask virusTask = new TimerTask() {
-      @Override
-      public void run() {
-        pointIncrement(10);
-        if (collision) {
-          pointsTimer.cancel();
-        }
-      }
+		};
+		cloudsTimer.scheduleAtFixedRate(cloudsTask, 1000L, l);
+	}
 
-    };
-    pointsTimer.scheduleAtFixedRate(virusTask, 1000L, 1000L);
-  }
+	// This method is called in runWithTimer, and is in charge of running code
+	// related to the points system
+	private void runPoints() {
+		TimerTask virusTask = new TimerTask() {
+			@Override
+			public void run() {
+				pointIncrement(10);
+				if (collision) {
+					pointsTimer.cancel();
+				}
+			}
 
-  // In charge of painting all components
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g); // Call JPanel's paintComponent method to paint
-    // the background
+		};
+		pointsTimer.scheduleAtFixedRate(virusTask, 1000L, 1000L);
+	}
 
-    int width = getWidth();
-    int height = getHeight();
+	// In charge of painting all components
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g); // Call JPanel's paintComponent method to paint
+		// the background
 
-    double ratioX = (double) width / DRAWING_WIDTH;
-    double ratioY = (double) height / DRAWING_HEIGHT;
+		int width = getWidth();
+		int height = getHeight();
 
-    Graphics2D g2 = (Graphics2D) g;
-    AffineTransform at = g2.getTransform();
-    g2.scale(ratioX, ratioY);
+		double ratioX = (double) width / DRAWING_WIDTH;
+		double ratioY = (double) height / DRAWING_HEIGHT;
 
-    virus.draw(g, this);
-    mahaf.draw(g, this);
-    cloud.draw(g, this);
-    cloud1.draw(g, this);
-    cloud2.draw(g, this);
-    cloud3.draw(g, this);
-    grass.draw(g, this);
+		Graphics2D g2 = (Graphics2D) g;
+		AffineTransform at = g2.getTransform();
+		g2.scale(ratioX, ratioY);
 
-    g2.setTransform(at);
+		virus.draw(g, this);
+		mahaf.draw(g, this);
+		cloud.draw(g, this);
+		cloud1.draw(g, this);
+		cloud2.draw(g, this);
+		cloud3.draw(g, this);
+		grass.draw(g, this);
 
-    g.setColor(Color.BLACK);
-    g.setFont(new Font("SansSerif", Font.BOLD, 12));
-    FontMetrics fm = g.getFontMetrics();
-    String p = "points: " + points;
-    g.drawString(p, width - fm.stringWidth(p) - 10, 20);
+		g2.setTransform(at);
 
-    if (collision) {
-      g.setFont(new Font("SansSerif", Font.BOLD, 30));
-      fm = g.getFontMetrics();
-      String s = "You caught the virus! GAME OVER";
-      g.drawString(s, width / 2 - fm.stringWidth(s) / 2, height / 2);
-      gameOver.play();
-    }
-    
-    if (hasMask) {
-      g.setFont(new Font("SansSerif", Font.BOLD, 12));
-      String m = "You have a mask!";
-      g.drawString(m, width - fm.stringWidth(p) - 120, 20);
-    }
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("SansSerif", Font.BOLD, 12));
+		FontMetrics fm = g.getFontMetrics();
+		String p = "points: " + points;
+		g.drawString(p, width - fm.stringWidth(p) - 10, 20);
 
-  }
+		if (collision) {
+			g.setFont(new Font("SansSerif", Font.BOLD, 30));
+			fm = g.getFontMetrics();
+			String s = "You caught the virus! GAME OVER";
+			g.drawString(s, width / 2 - fm.stringWidth(s) / 2, height / 2);
+			gameOver.play();
+		}
 
-  // Methods of Key Listener, explains what happens when keys are pressed
-  public void keyPressed(KeyEvent e) {
-    if (collision) {
-      return;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_SPACE) {
-      mahaf.jump();
-    }
-  }
+		if (hasMask) {
+			g.setFont(new Font("SansSerif", Font.BOLD, 12));
+			String m = "You have a mask!";
+			g.drawString(m, width - fm.stringWidth(p) - 120, 20);
+		}
 
-  // Methods of Key Listener, explains what happens when keys are released
-  public void keyReleased(KeyEvent e) {
-    // method needed by key listener
-  }
+	}
 
-  public void keyTyped(KeyEvent e) {
-  }
+	// Methods of Key Listener, explains what happens when keys are pressed
+	public void keyPressed(KeyEvent e) {
+		if (collision) {
+			return;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_SPACE) {
+			mahaf.jump();
+		}
+	}
 
-  public void checkmahaf() {
-    int x = mahaf.getX() + mahaf.getWidth() / 2;
-    int y = mahaf.getY() + mahaf.getHeight() / 2;
-    if (x < 0 || x > DRAWING_WIDTH || y < 0 || y > DRAWING_HEIGHT)
-      mahaf = new Player(380, 0);
-  }
+	// Methods of Key Listener, explains what happens when keys are released
+	public void keyReleased(KeyEvent e) {
+		// method needed by key listener
+	}
 
-  public boolean checkCollision() {
-    if (mahaf.getX() + mahaf.getWidth() - 30 >= virus.getX() && mahaf.getX() <= virus.getX()
-        && mahaf.getY() + mahaf.getHeight() - 30 >= virus.getY() && mahaf.getY() <= virus.getY()
-        && hasMask == false
-            
-        ||
+	public void keyTyped(KeyEvent e) {
+	}
 
-        virus.getX() + virus.getWidth() - 30 >= mahaf.getX() && virus.getX() <= mahaf.getX()
-            && mahaf.getY() + mahaf.getHeight() - 30 >= virus.getY() && mahaf.getY() <= virus.getY()
-            && hasMask == false) {
+	public void checkmahaf() {
+		int x = mahaf.getX() + mahaf.getWidth() / 2;
+		int y = mahaf.getY() + mahaf.getHeight() / 2;
+		if (x < 0 || x > DRAWING_WIDTH || y < 0 || y > DRAWING_HEIGHT)
+			mahaf = new Player(380, 0);
+	}
 
-      hasMask = false;
-      return true;
-    }
+	public boolean checkCollision() {
+		if (mahaf.getX() + mahaf.getWidth() - 30 >= virus.getX() && mahaf.getX() <= virus.getX()
+				&& mahaf.getY() + mahaf.getHeight() - 30 >= virus.getY() && mahaf.getY() <= virus.getY()
+				&& hasMask == false
 
-    return false;
+				||
 
-  }
+				virus.getX() + virus.getWidth() - 30 >= mahaf.getX() && virus.getX() <= mahaf.getX()
+						&& mahaf.getY() + mahaf.getHeight() - 30 >= virus.getY() && mahaf.getY() <= virus.getY()
+						&& hasMask == false) {
 
-  public boolean checkMaskCollision() {
-    if (mahaf.getX() + mahaf.getWidth() - 30 >= mask.getX() && mahaf.getX() <= mask.getX()
-        && mahaf.getY() + mahaf.getHeight() - 30 >= mask.getY() && mahaf.getY() <= mask.getY()
+			hasMask = false;
+			return true;
+		}
 
-        ||
+		return false;
 
-        mask.getX() + mask.getWidth() - 30 >= mahaf.getX() && mask.getX() <= mahaf.getX()
-            && mahaf.getY() + mahaf.getHeight() - 30 >= mask.getY() && mahaf.getY() <= mask.getY()) {
+	}
 
-      return true;
-    }
+	public boolean checkMaskCollision() {
+		if (mahaf.getX() + mahaf.getWidth() - 30 >= mask.getX() && mahaf.getX() <= mask.getX()
+				&& mahaf.getY() + mahaf.getHeight() - 30 >= mask.getY() && mahaf.getY() <= mask.getY()
 
-    return false;
+				||
 
-  }
+				mask.getX() + mask.getWidth() - 30 >= mahaf.getX() && mask.getX() <= mahaf.getX()
+						&& mahaf.getY() + mahaf.getHeight() - 30 >= mask.getY() && mahaf.getY() <= mask.getY()) {
 
-  // used in order to add points
-  public int pointIncrement(int n) {
-    points += n;
-    return points;
-  }
+			return true;
+		}
 
-  // the main methods
-  public static void main(String[] args) {
-    JFrame w = new JFrame("PROgrammerS");
-    w.setBounds(100, 100, 640, 480);
-    w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Panel panel = new Panel();
-    w.addKeyListener(panel);
-    w.add(panel);
-    w.setResizable(false);
-    w.setVisible(true);
-    panel.runWithTimer();
-  }
+		return false;
+
+	}
+
+	// used in order to add points
+	public int pointIncrement(int n) {
+		points += n;
+		return points;
+	}
+
+	// the main methods
+	public static void main(String[] args) {
+		JFrame w = new JFrame("PROgrammerS");
+		w.setBounds(100, 100, 640, 480);
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Panel panel = new Panel();
+		w.addKeyListener(panel);
+		w.add(panel);
+		w.setResizable(false);
+		w.setVisible(true);
+		panel.runWithTimer();
+	}
 
 }
